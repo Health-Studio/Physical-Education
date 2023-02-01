f31e3d6c2c44a292a591877034b7c96fe93fc3b1
Guilherme <guipython2001@gmail.com>
2023-02-01 00:59:50 -0300

    fix: style fix with prettier

--- src/application/errors/application.error.ts
+++ src/application/errors/application.error.ts
@@ -1,25 +1,34 @@
 import NotificationError from "@src/domain/commons/notification/notification.error";
 
 export class ApplicationError extends Error {
-  private static withErrors(message: string, errors: { context: string; errors: string[] }[]): string{
+  private static withErrors(
+    message: string,
+    errors: { context: string; errors: string[] }[]
+  ): string {
     const messages = errors.map(
       (m) => `:\n${m.context}:\n${m.errors.map((e) => `\t${e}\n`)}`
     );
 
-    return message +
-      (messages.length == 0 ? "" : messages.reduce((v, acc) => acc + v));
+    return (
+      message +
+      (messages.length == 0 ? "" : messages.reduce((v, acc) => acc + v))
+    );
   }
 
-  private static withError(message: string, error: string): string{
-    return `${message}:\n\t${error}`
+  private static withError(message: string, error: string): string {
+    return `${message}:\n\t${error}`;
   }
 
   public constructor(
     public message: string,
     public errors: { context: string; errors: string[] }[],
-    public error: string,
+    public error: string
   ) {
-    super(errors.length == 0 ? ApplicationError.withError(message, error) : ApplicationError.withErrors(message,errors));
+    super(
+      errors.length == 0
+        ? ApplicationError.withError(message, error)
+        : ApplicationError.withErrors(message, errors)
+    );
 
     this.name = this.constructor.name;
     Error.captureStackTrace(this, this.constructor);
