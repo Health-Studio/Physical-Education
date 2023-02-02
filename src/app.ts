a902d0d517502b9a698ef2b6d9217843506e26eb
Guilherme <guipython2001@gmail.com>
2023-02-02 01:13:33 -0300

    add: graceful shutdown for app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,49 @@
-import { Server } from "./server";
+import Server from "./server";
 import dotenv from "dotenv";
+import logger from "./logger";
 
-dotenv.config();
+enum Status {
+  Failure = 1,
+  Success = 0,
+}
 
-const port = process.env.PORT;
-const host = process.env.HOST;
+process.on("unhandledRejection", (reason, promise) => {
+  logger.error(
+    `Exiting due to an unhandled promise: ${promise} reason: ${reason}`
+  );
+  throw reason;
+});
 
-const server = new Server(host, port);
-server.init();
+process.on("uncaughtException", (error) => {
+  logger.error(`Exiting due to an uncaught exception: ${error}`);
+  process.exit(Status.Failure);
+});
+
+(async (): Promise<void> => {
+  try {
+    dotenv.config();
+    process.env.LOG = "true";
+    const port = process.env.PORT;
+    const host = process.env.HOST;
+
+    const server = new Server(host, port);
+    server.init();
+
+    const signals: NodeJS.Signals[] = ["SIGINT", "SIGTERM", "SIGQUIT"];
+    signals.map((signal) =>
+      process.on(signal, async () => {
+        try {
+          await server.close();
+          logger.info("Exited with success");
+          process.exit(Status.Success);
+        } catch (error) {
+          logger.error(`Exited with error ${error}`);
+          process.exit(Status.Failure);
+        }
+      })
+    );
+  } catch (error) {
+    logger.error(`Exited with error ${error}`);
+    process.exit(Status.Failure);
+  }
+})();
