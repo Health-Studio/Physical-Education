14b36ff3f766aee87e2a59481c8a55b3fc8a4619
Guilherme <guipython2001@gmail.com>
2023-02-02 00:48:30 -0300

    build & config: add and config pino-pretty

--- src/logger.ts
+++ src/logger.ts
@@ -1,6 +1,12 @@
 import pino from "pino";
 
 export default pino({
-  enabled: true,
+  enabled: process.env.LOG === "true",
+  transport: {
+    target: "pino-pretty",
+    options: {
+      colorize: true,
+    },
+  },
   level: "info",
 });
